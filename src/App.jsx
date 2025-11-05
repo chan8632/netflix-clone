3e3bab6029a22f243818995cfa8405a004041c2c
chan8632 <duscks8632@gmail.com>
2025-11-05 12:07:47 +0900

    10-3 내가 한 거 초기세팅 및 설치, router 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layout/AppLayout";
+import HomePage from "./page/Home/HomePage";
+import MoviesPage from "./page/Movies/MoviesPage";
+import MovieDetailPage from "./page/MovieDetail/MovieDetailPage";
+import NotFoundPage from "./page/NotFoundPage/NotFoundPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // 홈페이지  (/)
+  // 전체영화 목록 (/movies)
+  // 영화 상세페이지 (/movies/:id)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/movies">
+          <Route index element={<MoviesPage />} />
+          <Route path="/:id" element={<MovieDetailPage />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
---
+++ src/page/MovieDetail/MovieDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MovieDetailPage = () => {
+  return (
+    <div>
+      영화 상세 페이지
+    </div>
+  )
+}
+
+export default MovieDetailPage
