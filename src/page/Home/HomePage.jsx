eb00f83fb5233b885d6561eeac33f6ad65720fae
chan8632 <duscks8632@gmail.com>
2025-11-06 16:22:36 +0900

    feat(home): PopularMovieSlide 및 MovieCard 구현
    - 기존의 범용 Slide 컴포넌트 이름을 PopularMovieSlide로 변경하여 용도를 명확히 합니다.
    - 개별 영화 포스터를 표시하기 위한 MovieCard 컴포넌트를 GBE합니다.
    - PopularMovieSlide 내부에서 영화 데이터를 MovieCard 컴포넌트로 맵핑하여 렌더링하도록 GBE합니다.
    - 캐러셀 설정을 GBE하여 더 많은 아이템(items: 8)을 표시하고 centerMode를 GBE합니다.
    `
    10-8 영화 슬라이드 만들기(채점)

--- src/page/Home/HomePage.jsx
+++ src/page/Home/HomePage.jsx
@@ -3,14 +3,13 @@
 // 평점 높은 영화
 
 import Banner from "./components/Banner/Banner";
-import Slide from "./components/Slide/Slide";
-import "react-multi-carousel/lib/styles.css";
+import PopularMovieSlide from "./components/PopularMovieSlide/PopularMovieSlide";
 // 최신 영화
 const HomePage = () => {
   return (
     <div>
       <Banner />
-      <Slide />
+      <PopularMovieSlide />
     </div>
   );
 };
--- src/page/Home/components/PopularMovieSlide/PopularMovieSlide.jsx
+++ src/page/Home/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -2,26 +2,25 @@ import React from "react";
 import Carousel from "react-multi-carousel";
 import { usePopularMoviesQuery } from "../../../../hooks/usePopularMovie";
 import { Alert } from "react-bootstrap";
+import "react-multi-carousel/lib/styles.css";
+import MovieCard from "../MovieCard/MovieCard";
 
-const Slide = () => {
+const PopularMovieSlide = () => {
   const { isLoading, data, isError, error } = usePopularMoviesQuery();
   if (isLoading) return <div>isLoading</div>;
   if (isError) return <Alert variant={"danger"}>{error.message}</Alert>;
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-      slidesToSlide: 3, // optional, default to 1.
+      items: 8,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
-      slidesToSlide: 2, // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 1,
-      slidesToSlide: 1, // optional, default to 1.
     },
   };
   console.log(data.results);
@@ -29,19 +28,18 @@ const Slide = () => {
     <div>
       <h3>Popuplar Movies</h3>
       <Carousel
-        swipeable={false}
-        draggable={false}
-        responsive={responsive}
         infinite={true}
+        centerMode={true}
         containerClass="carousel-container"
-        removeArrowOnDeviceType={["tablet", "mobile"]}
+        itemClass="move-slider p-1"
+        responsive={responsive}
       >
-        {data.results.map((item) => (
-          <div>{item.title}</div>
+        {data.results.map((movie, index) => (
+          <MovieCard movie={movie} key={index} />
         ))}
       </Carousel>
     </div>
   );
 };
 
-export default Slide;
+export default PopularMovieSlide;
