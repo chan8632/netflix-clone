17a48e66f660beaf9589c387470d19a70645eeca
chan8632 <duscks8632@gmail.com>
2025-11-07 11:38:15 +0900

    Feat: 영화 카드 UI 구현 및 슬라이드 설정 변경 MovieCard: movie prop을 받도록 수정하고, 영화 포스터, 제목, 장르, 평점 등 상세 정보를 표시하도록 UI를 구현했습니다.
    MovieCard.style.css: 신규 CSS 파일을 추가하여, 카드 호버 시 줌 효과와 오버레이를 적용했습니다.
    
    PopularMovieSlide: MovieCard UI 변경에 따라 데스크톱 뷰의 아이템(items) 개수를 8개에서 4개로 조정했습니다.
    10-9 (내가함)

--- src/page/Home/components/MovieCard/MovieCard.jsx
+++ src/page/Home/components/MovieCard/MovieCard.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Badge } from "react-bootstrap";
+import "./MovieCard.style.css";
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
+  console.log("mmm", movie);
   return (
     <div>
-      <div>Movie Card</div>
+      <div
+        style={{
+          background: `url(https://image.tmdb.org/t/p/w220_and_h330_face/${movie?.poster_path})`,
+        }}
+        className="movie-card"
+      >
+        <div className="overlay">
+          <h4>{movie?.title}</h4>
+          {movie?.genre_ids.map((id) => (
+            <Badge bg="danger">{id}</Badge>
+          ))}
+          <div>{movie?.vote_average}</div>
+          <div>{movie?.popularity}</div>
+          <div>{movie?.adult ? "adult" : "under 18"}</div>
+        </div>
+      </div>
     </div>
   );
 };
