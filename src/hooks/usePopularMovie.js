b6588b02188836c2082d3bb8a4ef2ad3d74da884
chan8632 <duscks8632@gmail.com>
2025-11-09 10:49:15 +0900

    파일경로 불일치

--- src/hooks/usePopularMovie.js
+++ src/hooks/usePopularMovie.js
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const fetchPopularMovies = async () => {
-  return await axios.get("/./netlify/functions/getPopularMovies");
+  return await axios.get("/.netlify/functions/getPopularMovies");
 };
 export const usePopularMoviesQuery = () =>
   useQuery({
