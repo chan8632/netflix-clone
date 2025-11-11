97bd5595d34eeea4bf923e8e1f7895b08e736afd
chan8632 <duscks8632@gmail.com>
2025-11-11 11:57:37 +0900

    useSearchMovie 매개변수 객체로 변경

--- src/hooks/useSearchMovie.js
+++ src/hooks/useSearchMovie.js
@@ -1,16 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { api } from "../utils/api";
 
-const fetchSearchMovie = (keyword) => {
-  return keyword === null
-    ? api.get("movie/popular")
-    : api.get(`search/movie?query=${keyword}`);
+const fetchSearchMovie = ({ keyword }) => {
+  return keyword
+    ? api.get(`search/movie?query=${keyword}`)
+    : api.get("movie/popular");
 };
 
-export const useSearchMovie = (keyword) => {
+export const useSearchMovie = ({ keyword }) => {
   return useQuery({
     queryKey: ["search-data", keyword],
-    queryFn: () => fetchSearchMovie(keyword),
+    queryFn: () => fetchSearchMovie({ keyword }),
     select: (result) => result.data,
   });
 };
