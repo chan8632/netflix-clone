f8828221ae2b41b0b8f6e9d50cf7b812b192fa26
chan8632 <duscks8632@gmail.com>
2025-11-14 01:23:07 +0900

    영화 페이지 장르 필터링 기능 준비

--- src/page/Movies/components/FilterButton.jsx
+++ src/page/Movies/components/FilterButton.jsx
@@ -4,22 +4,24 @@ import "./FilterButton.style.css";
 
 const FilterButton = ({ title, items, setSortRule }) => {
   return (
-    <Dropdown className="filter-button">
-      <Dropdown.Toggle variant="danger" id="dropdown-basic">
-        {title}
-      </Dropdown.Toggle>
+    <>
+      <Dropdown className="filter-button">
+        <Dropdown.Toggle variant="danger" id="dropdown-basic">
+          {title}
+        </Dropdown.Toggle>
 
-      <Dropdown.Menu>
-        {items.map((item, index) => (
-          <Dropdown.Item
-            onClick={(e) => setSortRule(e.target.innerText)}
-            key={index}
-          >
-            {item}
-          </Dropdown.Item>
-        ))}
-      </Dropdown.Menu>
-    </Dropdown>
+        <Dropdown.Menu>
+          {items.map((item, index) => (
+            <Dropdown.Item
+              onClick={(e) => setSortRule(e.target.innerText)}
+              key={index}
+            >
+              {item}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
+    </>
   );
 };
 
