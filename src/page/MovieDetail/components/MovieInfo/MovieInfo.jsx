b7b82eab1fe7267a1f8f637458df46d4eb22820e
chan8632 <duscks8632@gmail.com>
2025-11-15 00:24:00 +0900

    fix(MovieInfo): 포스터 이미지 에러 핸들링 및 크기 고정
    - 영화 포스터 이미지가 로드에 실패할 경우, 미리 준비된 fallback 이미지(errorImg)가 표시되도록 onError 핸들러를 추가했습니다.
    - 이미지 태그에 width: 300px, height: 450px의 고정 스타일을 적용하여, 이미지가 없는 경우에도 레이아웃이 깨지지 않도록 수정했습니다.

--- src/page/MovieDetail/components/MovieInfo/MovieInfo.jsx
+++ src/page/MovieDetail/components/MovieInfo/MovieInfo.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import InfoText from "../InfoText/InfoText";
-
+import errorImg from "../../../../assets/imageError.png";
 const MovieInfo = ({ movieData }) => {
+  const handleImgError = (e) => (e.target.src = errorImg);
   return (
     <Container className="mb-5">
       <Row>
         <Col xs={12} md={6} className="d-flex justify-content-center">
           <img
+            style={{
+              width: "300px",
+              height: "450px",
+            }}
             src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${movieData?.poster_path}`}
+            onError={handleImgError}
           />
         </Col>
         <Col xs={12} md={6} className="d-flex justify-content-center">
